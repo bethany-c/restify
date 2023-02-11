b37703d61c88cfa6c02b04c406eaeecbfae0965a
QIyi Zhang <qiyi.zhang@mai.utoronto.ca>
2023-02-10 22:38:37 -0500

    123

--- P1/listing.js
+++ P1/listing.js
@@ -27,30 +27,30 @@ function increment_g() {
        }
 
 
-function display(){
+// function display(){
 
-    var files = document.getElementById("files").files;
-    var output = document.getElementById("result");
+//     var files = document.getElementById("files").files;
+//     var output = document.getElementById("result");
 
-    for (var i = 0; i < files.length; i++) {
-            var file = files[i];
-            //Only pics
-            if (!file.type.match('image'))
-              continue;
-            var picReader = new FileReader();
-            picReader.addEventListener("load", function(event) {
-              var picFile = event.target;
-              var div = document.createElement("div");
-              div.innerHTML = "<img class='thumbnail' src='" + picFile.result + "'" +
-                "title='" + picFile.name + "'/>";
-                  output.insertBefore(div, null);
-            });
-            //Read the image
-            picReader.readAsDataURL(file);
+//     for (var i = 0; i < files.length; i++) {
+//             var file = files[i];
+//             //Only pics
+//             if (!file.type.match('image'))
+//               continue;
+//             var picReader = new FileReader();
+//             picReader.addEventListener("load", function(event) {
+//               var picFile = event.target;
+//               var div = document.createElement("div");
+//               div.innerHTML = "<img class='thumbnail' src='" + picFile.result + "'" +
+//                 "title='" + picFile.name + "'/>";
+//                   output.insertBefore(div, null);
+//             });
+//             //Read the image
+//             picReader.readAsDataURL(file);
 
-          }
+//           }
 
-        }
+//         }
 
 
 
