4f7599985a57f856055360ef09df403644ab1544
Mustafa Imam <mustafa@Mustafas-MacBook-Pro.local>
2023-03-14 19:40:02 -0400

    bruh

---
+++ P2/startup.sh
@@ -0,0 +1,8 @@
+pip3 install django
+pip3 install virtualenv
+virtualenv venv
+source venv/bin/activate
+pip3 install djangorestframework-simplejwt 
+pip3 install django-multiselectfield
+pip3 install pillow
+pip3 install django-filter
