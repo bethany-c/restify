21132d0ac3668cb0c23d0953ac53f3771dc7ba8c
Mustafa Imam <mustafa@Mustafas-MacBook-Pro.local>
2023-03-14 19:57:27 -0400

    changes to cron and startup

--- P2/restify/cron.py
+++ P2/restify/cron.py
@@ -2,13 +2,16 @@ from datetime import datetime
 from webpages.models.reservation import Reservation
 
 now = datetime.now()
-completed_reservations = Reservation.objects.filter(status='AP', end_time__lte=now)
+completed_reservations = Reservation.objects.filter(end_time__lte=now)
 
 for reservation in completed_reservations:
-    reservation.status = 'CO'
-    
-    reservation.save()
-
-
-
-
+    if reservation.status == 'AP':
+        reservation.status = 'CO'
+        reservation.available_date.booked_for = False
+        reservation.available_date = None
+        reservation.save()
+    else: # when status=="AR" - when the host just didnt approve it 
+        reservation.status = 'DE'
+        reservation.available_date.booked_for = False
+        reservation.available_date = None
+        reservation.save()
--- P2/start.sh
+++
@@ -1,8 +0,0 @@
-pip3 install django
-pip3 install virtualenv
-virtualenv venv
-source venv/bin/activate
-pip3 install djangorestframework-simplejwt 
-pip3 install django-multiselectfield
-pip3 install pillow
-pip3 install django-filter
