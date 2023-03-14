a1aec5f072d3885c28246e0eb5a751fc322227c5
Qiyi Z <qiyi.zhang@mail.utoronto.ca>
2023-03-14 16:18:36 -0400

    startup fix

--- P2/startup.sh
+++ P2/startup.sh
@@ -1,5 +1,8 @@
 pip3 install django
+pip3 install virtualenv
+virtualenv venv
 pip3 install djangorestframework-simplejwt 
 pip3 install django-multiselectfield
 pip3 install pillow
 pip3 install django-filter
+source venv/bin/activate
