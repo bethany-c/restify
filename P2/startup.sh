69f694e58fbb588670cb4c29f662f95ddbaccb4e
zhan7393 <markus@markus.com>
2023-03-14 19:15:54 -0400

    Update files

--- P2/startup.sh
+++
@@ -1,8 +0,0 @@
-pip3 install django
-pip3 install virtualenv
-virtualenv venv
-pip3 install djangorestframework-simplejwt 
-pip3 install django-multiselectfield
-pip3 install pillow
-pip3 install django-filter
-source venv/bin/activate
