feb6e5c3bd0d3eb62e52ce76961320cf0254266e
Qiyi Z <qiyi.zhang@mail.utoronto.ca>
2023-03-16 16:03:32 -0400

    123

--- P2/startup.sh
+++ P2/startup.sh
@@ -1,11 +1,11 @@
 sudo apt update
-sudo apt --only-upgrade install python3
+sudo apt-get install python3
 sudo apt install python3-pip
-python -m pip  django
+python3 -m pip  django
 sudo apt install virtualenv
 virtualenv venv
 source venv/bin/activate
-python -m pip install djangorestframework-simplejwt 
-python -m pip install django-multiselectfield
-python -m pip install pillow
-python -m pip install django-filter
+python3 -m pip install djangorestframework-simplejwt 
+python3 -m pip install django-multiselectfield
+python3 -m pip install pillow
+python3 -m pip install django-filter
