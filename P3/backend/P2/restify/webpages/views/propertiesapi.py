fa01559138c16f1ae56e0a1658233ba3fb28d5d8
Qiyi Z <qiyi.zhang@mail.utoronto.ca>
2023-04-18 20:37:41 -0400

    recovered

--- P3/backend/P2/restify/webpages/views/propertiesapi.py
+++ P3/backend/P2/restify/webpages/views/propertiesapi.py
@@ -70,6 +70,10 @@ class CreateAvailableDateAPIView(CreateAPIView):
     serializer_class = PropertyTimeRangePriceHostOfferSerializer
 
     
+    permission_classes = [IsAuthenticated]
+    serializer_class = PropertyTimeRangePriceHostOfferSerializer
+
+    
     def perform_create(self, serializer):
         p = get_object_or_404(Property, id=self.kwargs['pk'])
         if p.property_owner != self.request.user:
@@ -81,9 +85,11 @@ class CreateAvailableDateAPIView(CreateAPIView):
         start_date = serializer.validated_data['start_date']
         end_date = serializer.validated_data['end_date']
 
-        overlap1 = RangePriceHostOffer.objects.filter(id=self.kwargs['pk'],start_date__gte=start_date, end_date__lte=end_date)
-        overlap2 = RangePriceHostOffer.objects.filter(id=self.kwargs['pk'],start_date__lte=start_date, end_date__lte=start_date)
-        overlap3 = RangePriceHostOffer.objects.filter(id=self.kwargs['pk'],start_date__gte=end_date, end_date__gte=end_date)
+        overlap1 = RangePriceHostOffer.objects.filter(property=self.kwargs['pk'],start_date__lte=start_date, end_date__gte=end_date)
+        overlap2 = RangePriceHostOffer.objects.filter(property=self.kwargs['pk'],start_date__gte=start_date, start_date__lte=end_date)
+        overlap3 = RangePriceHostOffer.objects.filter(property=self.kwargs['pk'],end_date__gte=start_date, end_date__lte=end_date)
+        overlap4 = RangePriceHostOffer.objects.filter(property=self.kwargs['pk'],start_date__gte=start_date, end_date__lte=end_date)
+
         # overlap3 = RangePriceHostOffer.objects.filter(start_date__gte=end_date, end_date__lte=end_date,
         #                                    start_date__gte=start_date, end_date__lte=start_date) # causing an error 
 
@@ -94,8 +100,11 @@ class CreateAvailableDateAPIView(CreateAPIView):
             raise ValidationError('This time range overlaps with an existing range')
         if overlap3:
             raise ValidationError('This time range overlaps with an existing range')
+        if overlap4:
+            raise ValidationError('This time range overlaps with an existing range')
         else: 
             return super().perform_create(serializer)
+    
     
 # class ImagePropertiesAPIView(CreateAPIView):
 #     serializer_class = PropertyImageSerializer
